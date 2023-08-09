52902ca79052e2b4ba6ae521420c38fbeabefe68
Emma Böcker <stckoverflw@gmail.com>
2023-08-09 12:08:04 +0200

    move secrets file

---
+++ secrets.nix
@@ -0,0 +1,25 @@
+let
+  userKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIh+tAKie4OOkzxIwprEcQHiaL4ifkJKcSeN3bytV1rZ stckoverflw@gmail.com" # desktop
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPiBYp+pjF/3Q6deVfH4uMqYg6y9YbK29qZ6kNyWLxil stckoverflw@gmail.com" # laptop
+  ];
+
+  systemKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII8+tB44MHGTih2DlHeNpnEYE2ah6/OS6lwcJQTGwrae root@nixos";
+
+  allKeys = userKeys ++ [systemKey];
+in {
+  "secrets/nextcloud-admin.age".publicKeys = allKeys;
+
+  "secrets/iws-token.age".publicKeys = allKeys;
+  "secrets/iws-rs.age".publicKeys = allKeys;
+  "secrets/mongo-root.age".publicKeys = allKeys;
+
+  "secrets/ris-tokens.age".publicKeys = allKeys;
+
+  "secrets/matrix-secret.age".publicKeys = allKeys;
+
+  "secrets/authentik-redis-password.age".publicKeys = allKeys;
+  "secrets/authentik-secrets.age".publicKeys = allKeys;
+
+  "secrets/grafana-oauth-secret.age".publicKeys = allKeys;
+}
