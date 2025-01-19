045d0fdd0ce9ab36a836e8e874898dccf142160c
Emma Böcker <emma@boecker.dev>
2025-01-19 17:36:26 +0100

    fix home assistant

--- hosts/july/applications/home-assistant.nix
+++ hosts/july/applications/home-assistant.nix
@@ -1,10 +1,11 @@
-{ ... }: {
+{ self, ... }: {
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w"
   ];
 
   services.home-assistant = {
     enable = true;
+    package = self.inputs.nixpkgs-unstable.legacyPackages.x86_64-linux.home-assistant;
 
     extraPackages = python3Packages: with python3Packages; [
       fritzconnection
@@ -14,20 +15,20 @@
       grpcio
       pychromecast
       nextcord
-      spotipy
+      spotifyaio
     ];
 
     config = null;
   };
 
-  services.nginx.tailscaleAuth = {
-    enable = true;
-    virtualHosts = ["hass.boecker.dev"];
-  };
+  # services.nginx.tailscaleAuth = {
+  #   enable = true;
+  #   virtualHosts = ["hass.boecker.dev"]; 
+  # };
   services.nginx.virtualHosts."hass.boecker.dev" = {
     locations."/" = {
       proxyPass = "http://localhost:8123";
-      proxyWebsockets = true;
+      proxyWebsockets = true; 
     };
   };
 }
